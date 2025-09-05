e4be1b0116c4cf72866128d7ad629afd36f8e91d
RobinsonG-source <guro1380@colorado.edu>
2025-09-05 09:48:02 -0600

    Update lab1fullcode.m
    Included problem 1 code

--- lab1fullcode.m
+++ lab1fullcode.m
@@ -4,6 +4,79 @@
 % Last Updated: 8/26/25
 
 clc; clear; close all;
+%% Q1 Parts A & B
+% For Part A calculate the solution to the dynamical system and plot the solution
+ic = [1; 1; 1; 1]; % Nonzero Initial Conditions
+
+% Generating the tspan and tolerance vectors
+tspan = [0 20];
+tol = [1e-2; 1e-4; 1e-6; 1e-8; 1e-10; 1e-12];
+
+% Vectors for saving the t=20 value for each tolerance
+finalWVec = [];
+finalXVec = [];
+finalYVec = [];
+finalZVec = [];
+
+% Calling ODE45, runs through each tolerance to get the needed values
+for i = 1:length(tol) % Runs through each tolerance and generates the solution 
+    options = odeset(RelTol = tol(i), AbsTol = tol(i));
+    [t, statevec] = ode45(@(t, statevec) odefnc(t, statevec), tspan, ic, options);
+    finalWVec = [finalWVec, statevec(end,1)];
+    finalXVec = [finalXVec, statevec(end,2)];
+    finalYVec = [finalYVec, statevec(end,3)];
+    finalZVec = [finalZVec, statevec(end,4)];
+
+    if i == 4 % Grabs the values for tol=1*10^-8 to plot
+        w = statevec(:, 1);
+        x = statevec(:, 2);
+        y = statevec(:, 3);
+        z = statevec(:, 4);
+
+        figure(1) % Subplot for the graphs
+        subplot(4, 1, 1)
+        plot(t, w) %plotting w vs t
+        title('w vs t')
+        xlabel('t (n.d.)')
+        ylabel('w (n.d.)')
+        subplot(4, 1, 2)
+        plot(t, x); %plotting x vs t
+        title('x vs t')
+        xlabel('t (n.d.)')
+        ylabel('x (n.d.)')
+        subplot(4, 1, 3)
+        plot(t, y); %plotting y vs t
+        title('y vs t')
+        xlabel('t (n.d.)')
+        ylabel('y (n.d.)')
+        subplot(4, 1, 4)
+        plot(t, z); %plotting z vs t
+        title('z vs t')
+        xlabel('t (n.d.)')
+        ylabel('z (n.d.)')
+        sgtitle('{\bf Question 1a, Dynamical System vs Time, Tolerances: 1x10^{-8}}', 'FontSize', 10)
+        print("Lab1Q1a", "-dpng");
+    end
+end
+
+%% Q1 Part B, finding differences between the tolerance and reference for the final value of the solution
+% Creating each field of the table 
+differenceTaken = ["w-w_r"; "x-x_r"; "y-y_r"; "z-z_r"];
+tol1 = [abs(finalWVec(1) - finalWVec(6)); abs(finalXVec(1) - finalXVec(6));...
+    abs(finalYVec(1) - finalYVec(6)); abs(finalZVec(1) - finalZVec(6))];%10^-2
+tol2 = [abs(finalWVec(2) - finalWVec(6)); abs(finalXVec(2) - finalXVec(6));...
+    abs(finalYVec(2) - finalYVec(6)); abs(finalZVec(2) - finalZVec(6))];%10^-4
+tol3 = [abs(finalWVec(3) - finalWVec(6)); abs(finalXVec(3) - finalXVec(6));...
+    abs(finalYVec(3) - finalYVec(6)); abs(finalZVec(3) - finalZVec(6))];%10^-6
+tol4 = [abs(finalWVec(4) - finalWVec(6)); abs(finalXVec(4) - finalXVec(6));...
+    abs(finalYVec(4) - finalYVec(6)); abs(finalZVec(4) - finalZVec(6))];%10^-8
+tol5 = [abs(finalWVec(5) - finalWVec(6)); abs(finalXVec(1) - finalXVec(6));...
+    abs(finalYVec(5) - finalYVec(6)); abs(finalZVec(5) - finalZVec(6))];%10^-10
+
+% Generating the table with appropriate rows and columns
+differences = table(differenceTaken, tol1, tol2, tol3, tol4, tol5);
+
+
 %% Q2 Parts B,C,D,E
 % Part A equation to base future parts off of
 % function xdot = objectEOM(t,x,rho,Cd,A,m,g,wind_vel) function from 2a
@@ -293,3 +366,20 @@ for i = 1:length(m)
     legendEntries{i} = ['m = ', num2str(m(i)), 'kg'];
 end
 legend(legendEntries)
+
+%% Functions
+% Function for problem 1 dynamical system
+function dstatevec_dt = odefnc(t, statevec)
+% Houses the derivative functions that we are trying to solve. Takes the
+% value of the statevector at that instance and solves the following
+% derivative values
+    w = statevec(1);
+    x = statevec(2);
+    y = statevec(3);
+    z = statevec(4);
+
+    dstatevec_dt = [-9*w + y; 
+                    4*w*x*y - x^2;
+                    2*w - x - 2*z; 
+                    x*y - y^2 - 3*(z^3)]; % dw/dt, dx/dt, dy/dt, dz/dt
+end
